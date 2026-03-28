007312882099630045dfe84c60a9c0495addcf41
Xiego-0 <diegodoamaral769@gmail.com>
2026-03-28 16:07:14 -0300

    Add files via upload

---
+++ v4_tabela_de_notas/script.js
@@ -0,0 +1,44 @@
+import { calcularMedia, definirStatus } from './opc.js'
+
+let trs = document.querySelectorAll('tbody tr')
+let input = document.querySelector('.entrada')
+
+
+
+
+for (let index = 0; index < trs.length; index++) {
+    let filho = trs[index].children
+
+    let nota1 = parseFloat(filho[1].textContent)
+    let nota2 = parseFloat(filho[2].textContent)
+    let nota3 = parseFloat(filho[3].textContent)
+
+    let tdStatus = filho[5]
+
+    let media = calcularMedia(nota1, nota2, nota3)
+    let tdMedia = filho[4]
+    tdMedia.textContent = media.toFixed(2)
+    let status = definirStatus(media, tdStatus)
+}
+
+// ITERADOR
+trs.forEach(tr => {
+    let filhos = tr.children
+    let status = filhos[5]
+
+    if (status.textContent == 'REPROVADO') {
+        tr.style.color = 'red'
+        tr.style.fontWeight = 'bold'
+    } else if (status.textContent == 'RECUPERAÇÃO') {
+        tr.style.backgroundColor = 'gray'
+        tr.style.fontSize = '15px'
+    } else {
+        tr.style.border = 'solid green 2px'
+    }
+})
+
+
+
+
+
+
